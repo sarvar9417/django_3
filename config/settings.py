aed8f11bb18b11b8fe51030c03f81d93d47a41c0
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2021-07-21 20:18:59 +0500

    Procfile added

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+4i^$d7=cn&*rd)bx#xswhs-8c_)&-wf3v-a2%%4+k%%)!i^n4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
